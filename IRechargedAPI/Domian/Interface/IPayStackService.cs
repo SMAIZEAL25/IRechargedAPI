2c69db279990e9042ec615b8dcfccd365ca280c4
smaizeal25 <samizeal25@gmail.com>
2025-05-31 19:50:54 +0100

    Ipaystack

---
+++ IRechargedAPI/Domian/Interface/IPayStackService.cs
@@ -0,0 +1,7 @@
+﻿namespace IRechargedAPI.Domian.Interface
+{
+    public interface IPayStackService
+    {
+        Task <(bool Success, string Message)> InitializePaymentAsync(decimal amount, string email, string reference);
+    }
+}
---
+++ IRechargedAPI/Infrastruture/BLL/AuthService/PayStackServices.cs
@@ -0,0 +1,19 @@
+﻿using IRechargedAPI.Domian.Interface;
+
+namespace IRechargedAPI.Infrastruture.BLL.AuthService
+{
+    public class PayStackServices : IPayStackService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _paystackBaseUrl = "https://api.paystack.co";
+
+        public PayStackServices(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public Task<(bool Success, string Message)> InitializePaymentAsync(decimal amount, string email, string reference)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
